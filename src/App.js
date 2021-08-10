921f6d828113f5c5a5ed60d74f14df52cb23d038
vinayak1823 <vinayak18dec@gmail.com>
2021-08-10 16:58:00 +0530

    Created the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Figure from './components/Figure'
+import Header from './components/Header'
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <div className='game-container'>
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
