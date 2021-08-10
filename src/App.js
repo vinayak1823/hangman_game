235b456013cee4f5f32f94fc05b5a6038a81473c
vinayak1823 <vinayak18dec@gmail.com>
2021-08-10 17:10:36 +0530

    Word displayed in word component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import Figure from './components/Figure'
 import Header from './components/Header'
 import WrongLetters from './components/WrongLetters'
 import Word from './components/Word'
+import { useState } from 'react'
+
+const words = ['application', 'programming', 'interface', 'wizard']
+
+let selectedWord = words[Math.floor(Math.random() * words.length)]
 
 function App() {
+  const [playable, setPlayable] = useState(true)
+  const [correctLetters, setCorrectLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+
   return (
     <>
       <Header />
       <div className='game-container'>
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   )
