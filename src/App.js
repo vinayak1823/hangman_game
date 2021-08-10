57538eac75de2ed7c2423075491301b023953784
vinayak1823 <vinayak18dec@gmail.com>
2021-08-10 17:38:41 +0530

    Event listener which handles the input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Figure from './components/Figure'
 import Header from './components/Header'
 import WrongLetters from './components/WrongLetters'
 import Word from './components/Word'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const words = ['application', 'programming', 'interface', 'wizard']
 
@@ -14,12 +14,40 @@ function App() {
   const [correctLetters, setCorrectLetters] = useState([])
   const [wrongLetters, setWrongLetters] = useState([])
 
+  useEffect(() => {
+    const handlekeydown = (e) => {
+      const { key, keyCode } = e
+
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase()
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter])
+          } else {
+            //showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((currentLetters) => [...currentLetters, letter])
+          } else {
+            //showNotification();
+          }
+        }
+      }
+    }
+
+    window.addEventListener('keydown', handlekeydown)
+
+    return () => window.removeEventListener('keydown', handlekeydown)
+  }, [correctLetters, wrongLetters, playable])
+
   return (
     <>
       <Header />
       <div className='game-container'>
         <Figure />
-        <WrongLetters />
+        <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
